128a3efc49f1a6541f287cfe4ed37d6a9bb51e3f
Gabriel Grijalva Flores <gabriel.grijalva@sinstec.com>
2021-11-01 12:21:43 -0500

    Implemented pending exchanges exports in index file.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+exports.BinanceCoin = require('./src/exchanges/binance-coin');
 exports.Bitmex = require('./src/exchanges/bitmex');
+exports.Bybit = require('./src/exchanges/bybit');
+exports.BybitFutures = require('./src/exchanges/bybit-futures');
+exports.Deribit = require('./src/exchanges/deribit');
+exports.KrakenFutures = require('./src/exchanges/kraken-futures');
+exports.OkexFutures = require('./src/exchanges/okex-futures');
