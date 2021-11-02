661393b6ead0d7158a54bd030b9c0a397427d942
Gabriel Grijalva Flores <gabriel.grijalva@sinstec.com>
2021-11-02 13:38:20 -0500

    Implemented ws-api types.

---
+++ types/ws-api.d.ts
@@ -0,0 +1,90 @@
+
+declare namespace WsApi {
+  import * as Events from 'events';
+  /**
+    * 
+    * 
+    * WS OPTIONS
+    * 
+    * 
+    */
+  type wsOptions = {
+    url?: string;
+    apiKey?: string;
+    apiSecret?: string;
+    apiPassphrase?: string;
+  }
+  /**
+   * 
+   * 
+   * WS ORDERS
+   * 
+   * 
+   */
+  type ordersParams = {
+    symbol: string;
+  }
+  type dataExecutions = {
+    id: string;
+    side: 'sell' | 'buy';
+    price: number;
+    quantity: number;
+    timestamp: string;
+  }[];
+  type dataCancelations = {
+    id: string;
+  }[];
+  type dataCreationsUpdates = {
+    id: string;
+    side: 'sell' | 'buy';
+    price: number;
+    quantity: number;
+  }[];
+  type ordersEventEmitter = Events.EventEmitter & {
+    // Emit 'event' functions
+    emit(event: 'executions', dataExecutions);
+    emit(event: 'cancelations', dataCancelations);
+    emit(event: 'creations-updates', dataCreationsUpdates);
+    // On 'event' functions
+    on(event: 'executions', listener: (data: dataExecutions) => void);
+    on(event: 'cancelations', listener: (data: dataCancelations) => void);
+    on(event: 'creations-updates', listener: (data: dataCreationsUpdates) => void);
+  }
+  /**
+   * 
+   * 
+   * WEBSOCKET INTERFACE
+   * 
+   * 
+   */
+  type webSocketOptions = {
+    url: string;
+  }
+  interface WebSocket {
+    // Util functions
+    send(data: string): void;
+    connect(url: string): void;
+    disconnect(): void;
+    // Add function listener;
+    addOnOpen(listener: () => void): void;
+    addOnClose(listener: () => void): void;
+    addOnError(listener: (error: string) => void): void;
+    addOnMessage(listener: (message: string) => void): void;
+    // Remove function listener;
+    removeOnOpen(listener: () => void): void;
+    removeOnClose(listener: () => void): void;
+    removeOnError(listener: (error: string) => void): void;
+    removeOnMessage(listener: (message: string) => void): void;
+  }
+  /**
+   * 
+   * 
+   * WS INTERFACE
+   * 
+   * 
+   */
+  interface Ws {
+    orders(params: ordersParams): ordersEventEmitter;
+  }
+
+}
