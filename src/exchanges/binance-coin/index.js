9c4fa9c8ff2dce33db5c54afade5b3828dd074be
Gabriel Grijalva Flores <gabriel.grijalva@sinstec.com>
2021-12-07 15:43:40 -0700

    Implemented populator in all exchanges.

--- src/exchanges/binance-coin/index.js
+++ src/exchanges/binance-coin/index.js
@@ -1,2 +1,3 @@
 exports.Rest = require('./_rest');
 exports.Ws = require('./_ws');
+exports.Populator = require('./_populator');
---
+++ src/exchanges/kraken-futures/_populator.js
@@ -0,0 +1,4 @@
+const Rest = require('./_rest');
+const Populator = require('../../_shared-classes/populator');
+
+module.exports = Populator(Rest());
