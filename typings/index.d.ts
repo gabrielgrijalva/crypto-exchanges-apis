f355fa0532a4f106c8b9f73518302bf828ceb7f2
Gabriel Grijalva Flores <gabriel.grijalva@sinstec.com>
2022-01-25 10:45:34 -0600

    Changed main library consumption settings and consumption.

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -11,13 +11,52 @@ import WsN = require('./_ws');
  * 
  * 
  */
-declare namespace ExchangeN {
-  interface Exchange {
-    Populator(options: PopulatorN.populatorOptions): PopulatorN.Populator;
-    Rest(options: RestN.restOptions): RestN.Rest;
-    Utils(options: UtilsN.utilsOptions): UtilsN.Utils;
-    Ws(options: WsN.wsOptions): WsN.Ws;
-  }
+type exchanges = 'binance-coin' | 'bitmex' | 'bybit' | 'bybit-futures' | 'deribit' | 'kraken-futures' | 'okex';
+type settings = {
+  EXCHANGE: exchanges,
+  SYMBOL: string,
+  API_KEY?: string,
+  API_SECRET?: string,
+  API_PASSPHRASE?: string,
+  INSTRUMENT: null | {
+    TYPE: 'spot' | 'future',
+    BALANCE_TYPE: 'base' | 'quote',
+    QUANTITY_TYPE: 'base' | 'quote',
+    PRICE_STEP: number,
+    QUANTITY_VALUE: number,
+    BASE_PRECISION: number,
+    QUOTE_PRECISION: number,
+    PRICE_PRECISION: number,
+    QUANTITY_PRECISION: number,
+  },
+  POPULATOR: null | {
+    PORT: number,
+    HOST: string,
+    USER: string,
+    DATABASE: string,
+    PASSWORD: string,
+    TIMEZONE: string,
+  },
+  REST: null | {
+    URL?: string,
+    REQUESTS_LIMIT: number,
+    REQUESTS_REFILL: number,
+    REQUESTS_REFILL_TYPE: '' | 'discrete' | 'continouos',
+    REQUESTS_REFILL_INTERVAL: number,
+    REQUESTS_TIMESTAMPS: number,
+  },
+  UTILS: null | {
+
+  },
+  WS: null | {
+    URL?: string,
+  },
+}
+interface CryptoExchangeApi {
+  populator: PopulatorN.Populator;
+  rest: RestN.Rest;
+  utils: UtilsN.Utils;
+  ws: WsN.Ws;
 }
 /**
  * 
@@ -28,6 +67,5 @@ declare namespace ExchangeN {
  * 
  * 
  */
-type exchanges = 'binance-coin' | 'bitmex' | 'bybit' | 'bybit-futures' | 'deribit' | 'kraken-futures' | 'okex';
-declare function CryptoExchangesApi(exchange: exchanges): ExchangeN.Exchange;
+declare function CryptoExchangesApi(settings: settings): CryptoExchangeApi;
 export = CryptoExchangesApi;
