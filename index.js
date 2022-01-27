9508d53af169d638acd1c553f1ec30f506eb4a5f
Gabriel Grijalva Flores <gabriel.grijalva@sinstec.com>
2022-01-27 11:01:47 -0600

    Changed main module export implementation.

--- index.js
+++ index.js
@@ -9,26 +9,26 @@ const Okex = require('./src/exchanges/okex');
  * @type {import('./typings')}
  */
 function CryptoExchangesApi(settings) {
-  if (exchange === 'binance-coin') {
-    return BinanceCoin;
+  if (settings.EXCHANGE === 'binance-coin') {
+    return BinanceCoin(settings);
   }
-  if (exchange === 'bitmex') {
-    return Bitmex;
+  if (settings.EXCHANGE === 'bitmex') {
+    return Bitmex(settings);
   }
-  if (exchange === 'bybit') {
-    return Bybit;
+  if (settings.EXCHANGE === 'bybit') {
+    return Bybit(settings);
   }
-  if (exchange === 'bybit-futures') {
-    return BybitFutures;
+  if (settings.EXCHANGE === 'bybit-futures') {
+    return BybitFutures(settings);
   }
-  if (exchange === 'deribit') {
-    return Deribit;
+  if (settings.EXCHANGE === 'deribit') {
+    return Deribit(settings);
   }
-  if (exchange === 'kraken-futures') {
-    return KrakenFutures;
+  if (settings.EXCHANGE === 'kraken-futures') {
+    return KrakenFutures(settings);
   }
-  if (exchange === 'okex') {
-    return Okex;
+  if (settings.EXCHANGE === 'okex') {
+    return Okex(settings);
   }
   throw new Error('Exchange not found.');
 };
