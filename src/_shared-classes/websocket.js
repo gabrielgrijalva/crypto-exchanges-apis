446f575f673d47ae770c8452c6013e55931d39e2
Gabriel Grijalva Flores <gabriel.grijalva@sinstec.com>
2022-03-15 11:26:34 -0500

    Corrected errors in websocket shared class implementation.

--- src/_shared-classes/websocket.js
+++ src/_shared-classes/websocket.js
@@ -7,6 +7,10 @@ function WebSocket() {
   let wsInstance = null;
   let webSocketErrors = 0;
   let websocketErrorsInterval = null;
+  const onOpenFunctions = [];
+  const onCloseFunctions = [];
+  const onErrorFunctions = [];
+  const onMessageFunctions = [];
   /**
    * 
    * 
@@ -87,6 +91,7 @@ function WebSocket() {
     connect: (url, options) => {
       if (wsInstance) { webSocket.disconnect() };
       wsInstance = new ws(url, options);
+      // Default Functions
       wsInstance.on('open', errorResetFunction);
       wsInstance.on('open', pingPongFunction(wsInstance));
       wsInstance.on('open', wsEventLogFunction(url, 'open'));
@@ -94,33 +99,50 @@ function WebSocket() {
       wsInstance.on('close', wsEventLogFunction(url, 'close'));
       wsInstance.on('error', errorHandlerFunction);
       wsInstance.on('error', wsEventLogFunction(url, 'error'));
+      // WebSocket added functions
+      onOpenFunctions.forEach(v => wsInstance.on('open', v));
+      onCloseFunctions.forEach(v => wsInstance.on('close', v));
+      onErrorFunctions.forEach(v => wsInstance.on('error', v));
+      onMessageFunctions.forEach(v => wsInstance.on('message', v));
     },
     close: closeFunction,
     disconnect: disconnectFunction,
     // Add function listener
     addOnOpen: (listener) => {
+      onOpenFunctions.push(listener);
       wsInstance ? wsInstance.on('open', listener) : null;
     },
     addOnClose: (listener) => {
+      onCloseFunctions.push(listener);
       wsInstance ? wsInstance.on('close', listener) : null;
     },
     addOnError: (listener) => {
+      onErrorFunctions.push(listener);
       wsInstance ? wsInstance.on('error', listener) : null;
     },
     addOnMessage: (listener) => {
+      onMessageFunctions.push(listener);
       wsInstance ? wsInstance.on('message', listener) : null;
     },
     // Remove function listener
     removeOnOpen: (listener) => {
+      const index = onOpenFunctions.findIndex(v => v === listener);
+      onOpenFunctions.splice(index, index !== -1 ? 1 : 0);
       wsInstance ? wsInstance.removeListener('open', listener) : null;
     },
     removeOnClose: (listener) => {
+      const index = onCloseFunctions.findIndex(v => v === listener);
+      onCloseFunctions.splice(index, index !== -1 ? 1 : 0);
       wsInstance ? wsInstance.removeListener('close', listener) : null;
     },
     removeOnError: (listener) => {
+      const index = onErrorFunctions.findIndex(v => v === listener);
+      onErrorFunctions.splice(index, index !== -1 ? 1 : 0);
       wsInstance ? wsInstance.removeListener('error', listener) : null;
     },
     removeOnMessage: (listener) => {
+      const index = onMessageFunctions.findIndex(v => v === listener);
+      onMessageFunctions.splice(index, index !== -1 ? 1 : 0);
       wsInstance ? wsInstance.removeListener('message', listener) : null;
     },
   };
