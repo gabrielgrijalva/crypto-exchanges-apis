8eaab5cc0b7055aa1e6fa47994fc6d198432268f
Gabriel Grijalva Flores <gabriel.grijalva@sinstec.com>
2022-04-05 19:48:07 -0500

    Corrected error in index typing declaration.

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -14,11 +14,11 @@ import WsN = require('./_ws');
  */
 type exchanges = 'binance-coin' | 'bitmex' | 'bitstamp' | 'bybit' | 'bybit-futures' | 'deribit' | 'kraken-futures' | 'okex';
 interface CryptoExchangeApi {
-  FIXER: FixerN.Fixer;
-  POPULATOR: PopulatorN.Populator;
-  REST: RestN.Rest;
-  UTILS: UtilsN.Utils;
-  WS: WsN.Ws;
+  FIXER(settings: FixerN.fixerSettings): FixerN.Fixer;
+  POPULATOR(settings: PopulatorN.populatorSettings): PopulatorN.Populator;
+  REST(settings: RestN.restSettings): RestN.Rest;
+  UTILS(settings: UtilsN.utilsSettings): UtilsN.Utils;
+  WS(settings: WsN.wsSettings): WsN.Ws;
 }
 declare function CryptoExchangesApi(exchange: exchanges): CryptoExchangeApi;
 export = CryptoExchangesApi;
