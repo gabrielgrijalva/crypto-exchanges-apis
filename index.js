dcfaaf93d8873c0e48c9c2a68a574ff525ea1cb2
Gabriel Grijalva Flores <gabriel.grijalva@sinstec.com>
2022-04-06 15:20:59 -0500

    Corrected error when loading exchange modules.

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ const exchanges = [
 function CryptoExchangesApi(exchange) {
   if (!exchanges.find(v => v === exchange)) throw new Error('Exchange not found.');
   return {
-    FIXER: require(`./src/exchanges/${exchange}/_fixer`),
-    POPULATOR: require(`./src/exchanges/${exchange}/_populator`),
-    REST: require(`./src/exchanges/${exchange}/_rest`),
-    UTILS: require(`./src/exchanges/${exchange}/_utils`),
-    WS: require(`./src/exchanges/${exchange}/_ws`),
+    FIXER: (() => { try { require(`./src/exchanges/${exchange}/_fixer`) } catch (err) { return null } })(),
+    POPULATOR: (() => { try { require(`./src/exchanges/${exchange}/_populator`) } catch (err) { return null } })(),
+    REST: (() => { try { require(`./src/exchanges/${exchange}/_rest`) } catch (err) { return null } })(),
+    UTILS: (() => { try { require(`./src/exchanges/${exchange}/_utils`) } catch (err) { return null } })(),
+    WS: (() => { try { require(`./src/exchanges/${exchange}/_ws`) } catch (err) { return null } })(),
   }
 };
 module.exports = CryptoExchangesApi;
