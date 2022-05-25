877ea37e67596e63d26a852af4c095716e2af307
Gabriel Grijalva Flores <gabriel.grijalva@sinstec.com>
2022-05-25 15:22:33 -0500

    Changed implementation of server and client order books.

--- src/_shared-classes/order-books-data-client.js
+++ src/_shared-classes/order-books-data-client.js
@@ -5,12 +5,13 @@ function OrderBooksDataClient(orderBooksWs) {
   /** @type {import('../../typings/_ws').orderBooksClientWsObject} */
   const orderBooksDataClient = {
     connect: (params) => {
-      const webSocket = WebSocket('order-book-client');
-      webSocket.connect(`ws://${params.host}:${params.port}`);
-      webSocket.addOnMessage((message) => {
-        orderBooksWs.data = JSON.parse(message);
+      return new Promise(resolve => {
+        const webSocket = WebSocket('order-book-client');
+        webSocket.connect(`ws://${params.host}:${params.port}`);
+        webSocket.addOnOpen(() => resolve());
+        webSocket.addOnMessage((message) => { orderBooksWs.data = JSON.parse(message) });
+        webSocket.addOnClose(() => { webSocket.connect(`ws://${params.host}:${params.port}`) });
       });
-      webSocket.addOnClose(() => { webSocket.connect(`ws://${params.host}:${params.port}`) });
     },
   };
   return orderBooksDataClient;
